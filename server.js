d4b4c751a1fb3bb1af9ae17e67aae4961b0e0f9a
mspstommy <44832675+mspstommy@users.noreply.github.com>
2019-07-12 23:39:01 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 var http = require("http");
 var fs = require("fs");
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  fs.readFile('input.txt', function (err, data) {
-    if (err) {
-        return console.error(err);
-    }
-    console.log("異步讀取: " + data.toString());
- });
-  response.write("Hello World");
+var server = http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write('Server online now');
   response.end();
-}).listen(8888);
+}).listen(10001);
+var mysql  = require('mysql');  
+var connection = mysql.createConnection({     
+  host     : 'localhost',       
+  user     : 'root',              
+  password : '123456',       
+  port: '10000',                   
+  database: 'work' 
+}); 
+ 
+connection.connect();
+ 
+var  sql = 'SELECT * FROM websites';
+connection.query(sql,function (err, result) {
+        if(err){
+          console.log('[SELECT ERROR] - ',err.message);
+          return;
+        }
+ 
+       console.log('--------------------------SELECT----------------------------');
+       console.log(result);
+       console.log('------------------------------------------------------------\n\n');  
+});
+ 
+connection.end();
