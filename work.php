c35c83b9570c73368bc9eebee02a6fc5b841d3f7
mspstommy <44832675+mspstommy@users.noreply.github.com>
2019-08-24 21:44:36 +0800

    Add files via upload

---
+++ work.php
@@ -0,0 +1,16 @@
+﻿<?php
+mysql_connect("localhost","root","crayon25");
+mysql_select_db("work");
+mysql_query("set names utf8");
+$data=mysql_query("select * from member");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>資料庫</title>
+</head>
+
+<body>
+</body>
+</html>
